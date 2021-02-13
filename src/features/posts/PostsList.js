d2c7abe5b0d7ccd9553816a433d5ed28a1600ff0
Tian <tian.qiu142@gmail.com>
2021-02-12 22:01:41 -0800

    completed PostsList component with redux selector and mapped posts

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { useSelector } from 'react-redux'
+
+export const PostsList = () => {
+  const posts = useSelector((state) => state.posts)
+
+  const mapPosts = posts.map((post) => (
+    <article>
+      <h4>post.title</h4>
+      <h6>post.content</h6>
+    </article>
+  ))
+
+  return (
+    <section>
+      <h3>Posts</h3>
+      <div>{mapPosts}</div>
+    </section>
+  )
+}
